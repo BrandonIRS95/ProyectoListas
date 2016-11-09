330ac80d9a1b2047f56a83af8ff9ec65bd62311c
DaliaPinto <dalia.pinto2@gmail.com>
2016-11-09 10:49:32 -0800

    First commit

---
+++ resources/views/includes/login.blade.php
@@ -0,0 +1,17 @@
+<!--Login partial view-->
+<div class="row">
+    <div class="col-md-4">
+        <h3>Inicio de Sesión</h3>
+        <form action="#" method="post">
+            <div class="form-group">
+                <label for="username">Usuario: </label>
+                <input type="text" class="form-control" name="username" id="username">
+            </div>
+            <div class="form-group">
+                <label for="username">Contraseña: </label>
+                <input type="password" class="form-control" name="password" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+    </div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--CSS-->
+        <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+        <title>Jerome</title>
+    </head>
+    <body>
+        @include('includes.navbar')
+        @include('includes.login')
+
+    </body>
+</html>
