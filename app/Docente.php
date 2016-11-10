2fa784d0e55fd0b4b8594050915763de45cab056
DaliaPinto <dalia.pinto2@gmail.com>
2016-11-09 17:06:49 -0800

    se agregó nuevo modelo docente

--- app/Docente.php
+++ app/Docente.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Docente extends Model
 {
-    //
+    //associate de user with Docente
+    public function user(){
+        return $this->hasOne('App\User');
+    }
 }
---
+++ database/migrations/2016_11_10_003223_create_docentes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocentesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('docentes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 40);
+            $table->string('apellido_paterno', 40);
+            $table->string('apellido_materno',40)->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('docentes');
+    }
+}
